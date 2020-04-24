1ec0fa02ade8d8f15035a6cda117a8c779def09f
cyrildewit <cyrilwit@gmail.com>
2020-04-24 19:51:14 +0200

    feat: add font

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -4,7 +4,7 @@ import MainLayoutFooter from './MainLayoutFooter'
 
 export default function MainLayout({ children }) {
     return (
-      <div className="text-gray-900 leading-normal">
+      <div className="font-body text-gray-900 leading-normal">
 
         <MainLayoutNav />
 
