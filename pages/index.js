e330ad6c6c6570fd5f39c24b67924c81cb25aa8c
cyrildewit <cyrilwit@gmail.com>
2020-04-25 21:38:32 +0200

    feat: update

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import MainLayout from '../components/MainLayout'
 function Home() {
   return (
     <MainLayout>
-      <div className="container mx-auto">
-        <div className="text-2xl">Homepage</div>
+      <div class="py-12 bg-gray-100 text-center border-b border-gray-300">
+        <div class="container mx-auto px-6">
+          <h1 class="text-2xl mb-2">sddfs</h1>
+          <p class="text-lg">sddfs</p>
+        </div>
       </div>
     </MainLayout>
   )
