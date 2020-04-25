9b7a81c8dd6bf9d726a2f808e0ab47eebe5d9bb6
cyrildewit <cyrilwit@gmail.com>
2020-04-25 21:35:58 +0200

    feat: add Footer

--- components/MainLayoutFooter.js
+++ components/MainLayoutFooter.js
@@ -1,7 +1,11 @@
 export default function MainLayoutFooter({ children }) {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div class="mt-4 border-t border-gray-400 flex flex-col items-center">
+        <div class="container mx-auto px-6">
+          <p class="py-6 text-sm text-center">&copy; { (new Date()).getFullYear()} by Leer Platform</p>
+        </div>
+      </div>
+    </footer>
   )
 }
