ce9efa090c7ebbd572faf3b64a6e92f004fc9a98
cyrildewit <cyrilwit@gmail.com>
2020-04-25 21:33:20 +0200

    feat: update MainLayoutNav

--- components/MainLayoutNav.js
+++ components/MainLayoutNav.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function MainLayoutNav({ children }) {
   return (
-    <nav className="bg-white shadow relative z-40">
+    <nav className="bg-white shadow relative z-40 border-t-4 border-purple-600">
       <div className="container mx-auto px-6">
 
         <div className="flex items-center justify-between h-20">
@@ -21,22 +21,28 @@ export default function MainLayoutNav({ children }) {
                 </a>
               </Link>
 
-              {/* <a class="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
-                Topics
-              </a> */}
+              <Link href="/catalog">
+                <a className="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
+                  Topics
+                </a>
+              </Link>
 
             </div>
           </div>
-        </div>
 
-        <div className="flex items-center">
-          <a className="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
-            Login
-          </a>
+          <div className="flex items-center">
+            <Link href="/login">
+              <a className="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
+                Login
+              </a>
+            </Link>
 
-          <a className="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
-            Register
-          </a>
+            <Link href="/register">
+              <a className="flex flex-row items-center px-3 py-2 font-medium rounded-md hover:text-gray-600 focus:outline-none focus:text-gray-800">
+                Register
+              </a>
+            </Link>
+          </div>
         </div>
 
       </div>
