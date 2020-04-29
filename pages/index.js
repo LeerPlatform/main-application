907630821b5c7e54d3997d8757e2339b9a6e4708
cyrildewit <cyrilwit@gmail.com>
2020-04-29 12:45:31 +0200

    feat: update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import MainLayout from '../components/MainLayout'
 import Link from 'next/link'
 import { getPopularTopics } from '../lib/topics'
+import { getPopularCourses } from '../lib/courses'
 import _ from 'lodash'
 
-function Home({ chunkedPopularTopics }) {
+function Home({ chunkedPopularTopics, chunkedPopularCourses }) {
   return (
     <MainLayout>
-      <div className="py-12 bg-gray-100 text-center border-b border-gray-300">
+      <div className="py-12 bg-white text-center border-b border-gray-300">
         <div className="container mx-auto px-4">
           <h1 className="text-3xl mb-2 font-medium">Welkom op het Leer Platform</h1>
           <p className="text-lg mb-6">Leer, ontdek en ontwikkel jouw talent!</p>
@@ -20,16 +21,19 @@ function Home({ chunkedPopularTopics }) {
       <div className="container mx-auto px-4">
         <div className="py-12">
 
-          <div></div>
-
           <div className="mb-4 pb-3 border-b border-gray-400">
-            <h2 className="font-medium text-lg">Populaire onderwerpen</h2>
+            <div className="flex items-end justify-between">
+              <h2 className="text-lg font-medium">Populaire onderwerpen</h2>
+              <Link href="/topics">
+                <a className="text-sm text-purple-900">Bekijk alles</a>
+              </Link>
+            </div>
           </div>
 
-          {chunkedPopularTopics.map(topicChunk => (
-            <div className="flex -mx-2">
+          {chunkedPopularTopics.map((topicChunk, index) => (
+            <div className="flex -mx-2" key={index.toString()}>
               {topicChunk.map(topic => (
-                <div className="w-1/3 px-2 mb-2">
+                <div className="w-1/3 px-2 mb-2" key={topic.id.toString()}>
                   <Link href="/test">
                     <a className="block px-3 py-3 bg-white hover:bg-gray-100 border shadow rounded-full font-medium text-center">
                       {topic.display_name}
@@ -43,56 +47,68 @@ function Home({ chunkedPopularTopics }) {
         </div>
       </div>
 
-      {/* <div className="container mx-auto px-4">
-        <div className="flex -mx-2">
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
+      <div className="container mx-auto px-4">
+        <div className="py-12">
+
+          <div className="mb-4 pb-3 border-b border-gray-400">
+            <div className="flex items-end justify-between">
+              <h2 className="text-lg font-medium">Populaire cursussen</h2>
+              <Link href="/catalog">
+                <a className="text-sm text-purple-900">Bekijk alles</a>
+              </Link>
+            </div>
           </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-400 h-12"></div>
-          </div>
-          <div className="w-1/12 px-2">
-            <div className="bg-gray-500 h-12"></div>
-          </div>
+
+          {chunkedPopularCourses.map((courseChunk, index) => (
+            <div className="flex -mx-2" key={index.toString()}>
+              {courseChunk.map(course => (
+                <div className="w-1/4 px-2 mb-2" key={course.id.toString()}>
+                  <div className="bg-white border shadow-sm rounded">
+                    <div className="overflow-hidden rounded-t">
+                      <img src="https://picsum.photos/id/203/320/180"/>
+                    </div>
+
+                    <div className="px-4 py-3">
+                      <h2 className="font-medium text-lg text-purple-800 mb-2">{ course.title }</h2>
+                      <p className="text-sm leading-snug">{ course.description_excerpt }</p>
+                    </div>
+
+                    <div className="px-4">
+                      {course.tags.map(tag => (
+                        <Link href="/test" key={tag.id.toString()}>
+                          <a className="text-xs font-medium">{ tag.name.en }</a>
+                        </Link>
+                      ))}
+                    </div>
+
+                    <div></div>
+                  </div>
+
+
+                  {/* <Link href="/test">
+                    <a className="block px-3 py-3 bg-white hover:bg-gray-100 border shadow rounded-full font-medium text-center">
+                      {topic.display_name}
+                    </a>
+                  </Link> */}
+                </div>
+              ))}
+            </div>
+          ))}
+
         </div>
-      </div> */}
+      </div>
     </MainLayout>
   )
 }
 
 export async function getStaticProps() {
   const popularTopics = await getPopularTopics()
+  const popularCourses = await getPopularCourses()
 
   return {
     props: {
       chunkedPopularTopics: _.chunk(popularTopics, 3),
+      chunkedPopularCourses: _.chunk(popularCourses, 3),
     },
   }
 }
