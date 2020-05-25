594ed2d0205f978ea728c815dbb90f9c6ec43a4a
cyrildewit <cyrilwit@gmail.com>
2020-05-25 17:05:57 +0200

    feat: set Inter font for body and add primary blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'body': ['Rubik', 'sans-serif'],
+        // 'body': ['Rubik', 'sans-serif'],
+        'body': ['Inter', 'serif'],
+      },
+      colors: {
+        'primary-blue': '#1089ff',
       },
     },
   },
