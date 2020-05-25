273f09a44ae7d93aecfad1ceb2eca1a53241f96a
cyrildewit <cyrilwit@gmail.com>
2020-05-25 17:26:12 +0200

    feat: improve main navigation

--- components/MainLayoutNav.js
+++ components/MainLayoutNav.js
@@ -2,42 +2,42 @@ import Link from 'next/link'
 import { authenticationService, userService } from '../services'
 
 export default function MainLayoutNav() {
-    const isAuthenticated = false;
+  const isAuthenticated = false;
 
   return (
     <nav className="bg-white shadow relative z-40 border-t-4 border-primary-blue">
       <div className="container mx-auto px-4">
 
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-16 lg:h-20">
           <div className="flex-shrink-0">
             <div className="flex items-center">
 
               <Link href="/">
                 <a className="flex flex-row items-center pr-3">
-                  <img src="/images/logo-icon-transparent-150x144.png" className="h-12" alt="Leer Platform Logo" />
+                  <img src="/images/logo-icon-transparent-150x144.png" className="h-8 lg:h-12" alt="Leer Platform Logo" />
                 </a>
               </Link>
 
               <Link href="/catalog">
-                <a className="flex flex-row items-center px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Catalogus
                 </a>
               </Link>
 
               <Link href="/topics">
-                <a className="flex flex-row items-center px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Onderwerpen
                 </a>
               </Link>
 
               <Link href="/news">
-                <a className="flex flex-row items-center px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Nieuws
                 </a>
               </Link>
 
               <Link href="/leaderbord">
-                <a className="flex flex-row items-center px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-3 py-2 rounded-md hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Leaderbord
                 </a>
               </Link>
@@ -48,13 +48,13 @@ export default function MainLayoutNav() {
           {!isAuthenticated &&
             <div className="flex items-center">
               <Link href="/login">
-                <a className="flex flex-row items-center px-4 py-2 mr-2 hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-4 py-2 mr-2 hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Log In
                 </a>
               </Link>
 
               <Link href="/register">
-              <a className="flex flex-row items-center px-4 py-2 border-2 border-primary-blue rounded-full hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
+                <a className="flex flex-row items-center px-2 lg:px-4 py-2 border-2 border-primary-blue rounded-full hover:text-primary-blue focus:outline-none focus:text-gray-800 transition ease-out duration-700">
                   Get Started
                 </a>
               </Link>
