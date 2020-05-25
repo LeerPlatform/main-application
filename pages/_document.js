a6bf74eeb35bc7dd4befd8cad167b6552121e103
cyrildewit <cyrilwit@gmail.com>
2020-05-25 17:05:40 +0200

    feat: import Inter font

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,12 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"
+          /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
