cfc2ca4cb630ad479262b1d5721369325f6acfc7
cyrildewit <cyrilwit@gmail.com>
2020-05-27 13:11:15 +0200

    feat: update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,25 @@ module.exports = {
       },
       colors: {
         'primary-blue': '#1089ff',
+        'primary-blue-dark': '#176dc1',
       },
       minHeight: {
         '20': '5rem',
-      }
+        '10': '2.5rem',
+        '12': '3rem',
+      },
+      filter: {
+        'brightness-80': 'brightness(80%)',
+      },
+      transitionProperty: {
+        'filter': 'filter',
+      },
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    filter: ['responsive', 'group-hover']
+  },
+  plugins: [
+    require('tailwindcss-filters'),
+  ],
 }
