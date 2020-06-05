af0d0f8c75912e2cd6f9e16cf5bb3ddd0043b711
cyrildewit <cyrilwit@gmail.com>
2020-06-05 21:34:43 +0200

    feat: wip sort

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -9,6 +9,7 @@ function Catalog({ initialResult, initialMeta, initialSearchQuery }) {
   const [courses, setCourses] = useState(initialResult)
   const [currentSearchQuery, setCurrentSearchQuery] = useState(initialSearchQuery)
   const [currentSearchResultCount, setCurrentSearchResultCount] = useState(initialMeta.total)
+  const [sortBy, setSortBy] = useState('-popular')
 
   function handleSearchChange(event) {
     setSearchQuery(event.target.value)
@@ -20,8 +21,17 @@ function Catalog({ initialResult, initialMeta, initialSearchQuery }) {
     }
   }
 
+  function handleSortByChange(event) {
+    let value = '-popular'
+    if (event.target.value === 'popular') { value = '-popular' }
+    if (event.target.value === 'latest') { value = '-created_at' }
+    setSortBy(value)
+
+    applySearch()
+  }
+
   async function applySearch() {
-    const { data, meta } = await fetchCourses({ query: searchQuery })
+    const { data, meta } = await fetchCourses({ query: searchQuery, sortBy })
 
     setCourses(data)
     setCurrentSearchQuery(searchQuery)
@@ -46,8 +56,8 @@ function Catalog({ initialResult, initialMeta, initialSearchQuery }) {
                   onKeyPress={handleSearchKeyPress}
                 />
 
-                <button type="submit" className="absolute left-0 top-0 mt-3 ml-4">
-                  <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{ enableBackgroundNew: '0 0 56.966 56.966'}} xmlSpace="preserve" width="512px" height="512px">
+                <button type="submit" className="absolute left-0 top-0 mt-3 ml-4 focus:outline-none text-gray-600" onClick={applySearch}>
+                  <svg className="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{ enableBackgroundNew: '0 0 56.966 56.966'}} xmlSpace="preserve" width="512px" height="512px">
                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
                   </svg>
                 </button>
@@ -60,11 +70,19 @@ function Catalog({ initialResult, initialMeta, initialSearchQuery }) {
 
             <div className="flex justify-between py-2 mb-4">
               <div>
-                <p className="text-sm">{currentSearchResultCount} resultaten {currentSearchQuery && (<>voor "{currentSearchQuery}"</>)}</p>
+                <p className="text-sm">{currentSearchResultCount > 0 ? `${currentSearchResultCount}` : 'Geen'} resultaten {currentSearchQuery && (<>voor "{currentSearchQuery}"</>)}</p>
               </div>
 
               <div>
-                Sorteren op...
+                <div className="inline-block relative w-64">
+                  <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" onChange={handleSortByChange}>
+                    <option value="popular" defaultChecked>Popular</option>
+                    <option value="latest">Latest</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                  </div>
+                </div>
               </div>
             </div>
 
@@ -107,7 +125,7 @@ function Catalog({ initialResult, initialMeta, initialSearchQuery }) {
   )
 }
 
-async function fetchCourses({query}) {
+async function fetchCourses({ query, sortBy }) {
   const params = {
     'include': ['authors', 'tags', 'language', 'studentsCount'],
     'page[size]': 16,
@@ -115,6 +133,10 @@ async function fetchCourses({query}) {
 
   if (typeof query === 'string') {
     params['filter[query]'] = query
+  }
+
+  if (typeof sortBy === 'string') {
+    params['sort'] = sortBy ?? '-popular'
   }
 
   return await courseService.getAll({ params })
